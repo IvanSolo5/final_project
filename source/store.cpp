a4f24867620de6c757776bd61d5468bd2279fa61
IvanSolo5 <45604966+IvanSolo5@users.noreply.github.com>
2018-12-12 23:39:56 -0800

    Update store.cpp

--- source/store.cpp
+++ source/store.cpp
@@ -1,10 +1,21 @@
+/////////////////////////////////////////////////////////
+/// Ivan Solodyankin & Simaon Mai
+/// CS2 Final Project
+/// Purpsoe: Create an base store 
+/// Project completd 12 December 2018
+//////////////////////////////////////////////////////////
 #include<iostream>
 #include<string>
 #include<vector>
+#include <fstream>
 #include "store.h"
+#include "burger.h"
+#include "taco.h"
+#include "pizza.h"
+#include "chinese.h"
+#include "icecream.h"
 
 using namespace std;
-
 
 store::store()
 {
@@ -20,31 +31,86 @@ store::store(string food, string drink)
     this->drinkItem = drink;
 }
 
-string store::getFood()
+
+void store::displayStore()
 {
-    return foodItem;
+        /// display options in food cour
+    cout << "Welcome to the Food Court!" << endl;
+    cout << "Where would you like to go?\n" << endl;
+    cout << "[1] \t Burger Stacks " << endl; 
+    cout << "[2] \t Taco Vector " << endl;
+    cout << "[3] \t Pizza Pi " << endl;
+    cout << "[4] \t Chinese Overload " << endl;
+    cout << "[5] \t Ice Cream Bytes \n" << endl;
 }
 
-string store::getDrink()
+void store::selectStore()
 {
-    return drinkItem;
-}
+    int uPick;
+    store s1;
+    burger b1;
+    taco t1;
+    chinese c1;
+    icecream i1;
+    pizza p1;
 
-double store::getFoodPrice()
-{
-    return foodItemPrice;
-}
+    cin >> uPick;
 
-double store::getDrinkPrice()
-{
-    return drinkItemPrice;
-}
-
-void store::makeOrder()
-{
-}
-
-void store::addTip()
-{
+    switch(uPick)
+    {
     
+        case 1: 
+        {
+            b1.selectBurger();
+            b1.selectDrink();
+            b1.displayOrder();
+            b1.receiptOutput();
+            break;
+        }
+            
+        case 2:
+        {
+           t1.selectTaco();
+           t1.selectDrink();
+           t1.displayOrder();
+           t1.receiptOutput();
+           break;
+        }
+            
+        case 3:
+        {
+            p1.selectPizza();
+            p1.selectDrink();
+            p1.displayOrder();
+            p1.receiptOutput();
+            break;
+        }
+           
+        case 4:
+        {
+            c1.selectChinese();
+            c1.selectDrink();
+            c1.displayOrder();
+            c1.receiptOutput();
+            break;
+        }
+            
+        case 5:
+        {
+            i1.selectIcecream();
+            i1.selectDrink();
+            i1.displayOrder();
+            i1.receiptOutput();
+            break;
+        }
+            
+        default:
+            cout << "Invalid entry" << endl;
+            break;
+
+    }
+
 }
+
+
+
