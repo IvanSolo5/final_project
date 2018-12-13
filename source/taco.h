ac203fa3b68dc70e98ed6075bb16a5be7e151cfb
IvanSolo5 <45604966+IvanSolo5@users.noreply.github.com>
2018-12-12 23:41:46 -0800

    Update taco.h

---
+++ source/taco.h
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////
+/// Ivan Solodyankin & Simaon Mai
+/// CS2 Final Project
+/// Purpsoe: Create an taco store that sells food and drink
+/// Project completd 12 December 2018
+//////////////////////////////////////////////////////////
+#include<iostream>
+#include<string>
+#include<vector>
+#include "store.h"
+
+
+using namespace std;
+
+#ifndef TACO
+#define TACO
+
+
+class taco
+{
+     private:
+        string taco_;
+        string tdrink_;
+       
+
+    public:
+        taco();
+        taco(string taco, double price);
+        vector<string> tacos;
+        vector<taco> torders;
+        string getTaco();
+        string getTdrink();
+        double tprice_, tdrinkPrice_;
+        double getPrice();
+        void selectTaco();
+        void displayVectorT(vector<taco> &);
+        double calcPrice();
+        void selectDrink();
+        void displayOrder();
+        void displayDrinksT(vector<string> d);
+        void receiptOutput();
+    
+};
+
+#endif 
