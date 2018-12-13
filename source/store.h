f371b450e92553fe39bfc9ba7fa27e823979ff55
IvanSolo5 <45604966+IvanSolo5@users.noreply.github.com>
2018-12-13 09:21:14 -0800

    Update store.h

--- source/store.h
+++ source/store.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////
-/// Ivan Solodyankin & Simaon Mai
+/// Ivan Solodyankin & Simon Mai
 /// CS2 Final Project
 /// Purpsoe: Create an base store 
 /// Project completd 12 December 2018
