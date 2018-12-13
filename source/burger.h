c26ee770146cf2b5882d50b5771dd17dea9325a0
IvanSolo5 <45604966+IvanSolo5@users.noreply.github.com>
2018-12-12 23:35:53 -0800

    Update burger.h

--- source/burger.h
+++ source/burger.h
@@ -1,15 +1,23 @@
+/////////////////////////////////////////////////////////
+/// Ivan Solodyankin & Simaon Mai
+/// CS2 Final Project
+/// Purpsoe: Create a buger store that sells food and drink
+/// Project completd 12 December 2018
+//////////////////////////////////////////////////////////
 #include<iostream>
 #include<string>
 #include<vector>
+#include "store.h"
 
 using namespace std;
 
-#ifndef BURGER_H
-#define BURGER_H
+#ifndef BURGER
+#define BURGER
 
-class burger 
+
+class burger
 {
-    private:
+     private:
         string burger_;
         string bdrink_;
        
@@ -18,15 +26,19 @@ class burger
         burger();
         burger(string burger, double price);
         vector<string> burgers;
-        vector<string> drinks;
+        vector<burger> orders;
         string getBurger();
         string getBdrink();
-        double price_;
+        double price_, drinkPrice_;
         double getPrice();
+        void selectBurger();
         void displayVector(vector<burger> &);
-        void displayDrinks(vector<string> );
         double calcPrice();
-
+        void selectDrink();
+        void displayOrder();
+        void displayDrinks(vector<string> d);
+        void receiptOutput();
+        
 
 };
 
